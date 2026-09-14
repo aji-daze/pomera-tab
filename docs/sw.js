369a62011582736ee98cde818f54802b29235300
aji-daze <touka.arisugawa.0619@gmail.com>
2026-09-14 12:49:22 +0900

    Add files via upload

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 // オフラインで起動できるよう、アプリ本体を端末に保存しておく。
 // アプリを更新したら VERSION を上げる（端末側に「更新があります」と表示される）。
-const VERSION = '1.3.0';
+const VERSION = '1.4.0';
 const CACHE = `pomera-tab-${VERSION}`;
 const FONT_CACHE = 'pomera-tab-fonts';
 const SHELL = [
